a10d8982162298d6eca5ddf7f99d718008b92788
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-14 10:11:51 -0600

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Jeremy Taylor on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
